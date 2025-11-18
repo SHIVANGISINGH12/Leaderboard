02dacee6c959f962403bf2e5efc6101b0f415b88
Shivangi Singh <shivangiarusingh@gmail.com>
2025-11-18 19:50:51 +0530

    Made the UI.

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,155 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
-  return (
-    <div className="container">
-      <h1>LEADERBOARD</h1>
-      <div className="inputContainer">
-        <input type="text" placeholder='First Name' ></input>
-        <input type="text" placeholder='Last Name' ></input>
-        <input type="text" placeholder='Country' ></input>
-        <input type="number" placeholder='Player Score' ></input>
-        <button className="addPlayer">Add Player</button>
-      </div>
-      <div className="board">
-        <div className="boardRow">
-          <div className="nameTime">
-            <p>Laxmi Sharma</p>
-            <p>5/22/2021, 3:21:57 PM</p>
-          </div>
-          <p className="country">India</p>
-          <p className="score">115</p>
-          <ion-icon name="trash-outline"></ion-icon>
-          <button className="increment">+5</button>
-          <button className="decrement">-5</button>
+    return (
+        <div className="container">
+            <h1>LEADERBOARD</h1>
+            <div className="inputContainer">
+                <input type="text" placeholder="First Name"></input>
+                <input type="text" placeholder="Last Name"></input>
+                <input type="text" placeholder="Country"></input>
+                <input type="number" placeholder="Player Score"></input>
+                <button className="addPlayer">Add Player</button>
+            </div>
+            <div className="board">
+                <div className="boardRow">
+                    <div className="nameTime">
+                        <p className="name">Laxmi Sharma</p>
+                        <p className="time">5/22/2021, 3:21:57 PM</p>
+                    </div>
+                    <p className="country">Infasfdfasdfdsadia</p>
+                    <p className="score">115</p>
+                    <div className="icon">
+                        <ion-icon name="trash-outline"></ion-icon>
+                    </div>
+                    <div className="buttonsContainer">
+                        <button className="increment">+5</button>
+                        <button className="decrement">-5</button>
+                    </div>
+                </div>
+                <div className="boardRow">
+                    <div className="nameTime">
+                        <p className="name">Laxmi Sharma</p>
+                        <p className="time">5/22/2021, 3:21:57 PM</p>
+                    </div>
+                    <p className="country">Infasfdfasdfdsadia</p>
+                    <p className="score">115</p>
+                    <div className="icon">
+                        <ion-icon name="trash-outline"></ion-icon>
+                    </div>
+                    <div className="buttonsContainer">
+                        <button className="increment">+5</button>
+                        <button className="decrement">-5</button>
+                    </div>
+                </div>
+                <div className="boardRow">
+                    <div className="nameTime">
+                        <p className="name">Laxmi Sharma</p>
+                        <p className="time">5/22/2021, 3:21:57 PM</p>
+                    </div>
+                    <p className="country">Infasfdfasdfdsadia</p>
+                    <p className="score">115</p>
+                    <div className="icon">
+                        <ion-icon name="trash-outline"></ion-icon>
+                    </div>
+                    <div className="buttonsContainer">
+                        <button className="increment">+5</button>
+                        <button className="decrement">-5</button>
+                    </div>
+                </div>
+                <div className="boardRow">
+                    <div className="nameTime">
+                        <p className="name">Laxmi Sharma</p>
+                        <p className="time">5/22/2021, 3:21:57 PM</p>
+                    </div>
+                    <p className="country">Infasfdfasdfdsadia</p>
+                    <p className="score">115</p>
+                    <div className="icon">
+                        <ion-icon name="trash-outline"></ion-icon>
+                    </div>
+                    <div className="buttonsContainer">
+                        <button className="increment">+5</button>
+                        <button className="decrement">-5</button>
+                    </div>
+                </div>
+                <div className="boardRow">
+                    <div className="nameTime">
+                        <p className="name">Laxmi Sharma</p>
+                        <p className="time">5/22/2021, 3:21:57 PM</p>
+                    </div>
+                    <p className="country">Infasfdfasdfdsadia</p>
+                    <p className="score">115</p>
+                    <div className="icon">
+                        <ion-icon name="trash-outline"></ion-icon>
+                    </div>
+                    <div className="buttonsContainer">
+                        <button className="increment">+5</button>
+                        <button className="decrement">-5</button>
+                    </div>
+                </div>
+                <div className="boardRow">
+                    <div className="nameTime">
+                        <p className="name">Laxmi Sharma</p>
+                        <p className="time">5/22/2021, 3:21:57 PM</p>
+                    </div>
+                    <p className="country">Infasfdfasdfdsadia</p>
+                    <p className="score">115</p>
+                    <div className="icon">
+                        <ion-icon name="trash-outline"></ion-icon>
+                    </div>
+                    <div className="buttonsContainer">
+                        <button className="increment">+5</button>
+                        <button className="decrement">-5</button>
+                    </div>
+                </div>
+                <div className="boardRow">
+                    <div className="nameTime">
+                        <p className="name">Laxmi Sharma</p>
+                        <p className="time">5/22/2021, 3:21:57 PM</p>
+                    </div>
+                    <p className="country">Infasfdfasdfdsadia</p>
+                    <p className="score">115</p>
+                    <div className="icon">
+                        <ion-icon name="trash-outline"></ion-icon>
+                    </div>
+                    <div className="buttonsContainer">
+                        <button className="increment">+5</button>
+                        <button className="decrement">-5</button>
+                    </div>
+                </div>
+                <div className="boardRow">
+                    <div className="nameTime">
+                        <p className="name">Laxmi Sharma</p>
+                        <p className="time">5/22/2021, 3:21:57 PM</p>
+                    </div>
+                    <p className="country">Infasfdfasdfdsadia</p>
+                    <p className="score">115</p>
+                    <div className="icon">
+                        <ion-icon name="trash-outline"></ion-icon>
+                    </div>
+                    <div className="buttonsContainer">
+                        <button className="increment">+5</button>
+                        <button className="decrement">-5</button>
+                    </div>
+                </div>
+                <div className="boardRow">
+                    <div className="nameTime">
+                        <p className="name">Laxmi Sharma</p>
+                        <p className="time">5/22/2021, 3:21:57 PM</p>
+                    </div>
+                    <p className="country">Infasfdfasdfdsadia</p>
+                    <p className="score">115</p>
+                    <div className="icon">
+                        <ion-icon name="trash-outline"></ion-icon>
+                    </div>
+                    <div className="buttonsContainer">
+                        <button className="increment">+5</button>
+                        <button className="decrement">-5</button>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    );
 }
 
-export default App
+export default App;
